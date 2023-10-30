2ae0422b41a02c4e338ad2151e5b7d0e50b6522e
Sebastian Avalos <sebas1208.avalos@gmail.com>
2023-10-30 11:46:17 -0500

    Almost fixes types

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -15,53 +15,54 @@ export function computed(f: Function) {
     activeEffect = f;
     const value = f();
     activeEffect = null;
-    return typeof value !== 'object' ? state({value}) : state(value)
+    return typeof value !== 'object' ? state({ value }) : state(value)
   }
   return effect();
 }
 
-const handler = {
-  set(target: object, key, newValue) {
-    if (newValue === target[key]) return true;
-    target[key] = newValue;
-    
-    const targetMap = reactiveMap.get(target);
-    const effectSet = targetMap?.get(key);
-    effectSet?.forEach(effect => effect());
-    console.log(reactiveMap.get(target));
-    
-    return true;
-  },
-  get(target: object, key: string) {
-    const value = target[key];
+function createHandler<T extends object>(): ProxyHandler<T> {
+  return {
+    set<T, K extends keyof T>(target: T, key: K, newValue: T[K]) {
+      if (newValue === target[key]) return true;
+      target[key] = newValue;
 
-    const targetMap = reactiveMap.get(target);
-    if(targetMap && activeEffect) {
-      const effects = targetMap.get(key);
-      if(!effects) {
+      const targetMap = reactiveMap.get(target);
+      const effectSet = targetMap?.get(String(key));
+      effectSet?.forEach(effect => effect());
+
+      return true;
+    },
+    get(target: object, key: keyof object) {
+      const value = target[key];
+
+      const targetMap = reactiveMap.get(target);
+      if (targetMap && activeEffect) {
+        const effects = targetMap.get(key);
+        if (!effects) {
+          const effectSet = new Set<Function>;
+          effectSet.add(activeEffect);
+          targetMap.set(key, effectSet);
+        }
+        effects?.add(activeEffect);
+      } else if (!targetMap && activeEffect) {
+        const effectMap = new Map<string, Set<Function>>;
+        reactiveMap.set(target, effectMap)
         const effectSet = new Set<Function>;
+        effectMap.set(key, effectSet);
         effectSet.add(activeEffect);
-        targetMap.set(key, effectSet);
       }
-      effects?.add(activeEffect);
-    } else if(!targetMap && activeEffect) {
-      const effectMap = new Map<string, Set<Function>>;
-      reactiveMap.set(target, effectMap)
-      const effectSet = new Set<Function>;
-      effectMap.set(key, effectSet);
-      effectSet.add(activeEffect);
-    }
 
-    return value;
+      return value;
+    }
   }
 }
 
 export function state<T extends object>(initialValue: T): T {
   Object.entries(initialValue).forEach(([key, value]) => {
     if (typeof value === 'object') {
-      initialValue[key] = state(value);
+      initialValue[(key as keyof T)] = state(value);
     }
   });
 
-  return new Proxy<T>(initialValue, handler);
+  return new Proxy<T>(initialValue, createHandler());
 }
